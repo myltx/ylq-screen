cd2f903bcc48162564dbc7df78949ff8214afe74
myltx <summmayl@gmail.com>
2025-03-04 15:45:10 +0800

    feat: 更新接收材料页面底部组件，添加车牌号、材料名称、进场数量等信息展示

--- src/views/receiveMaterials/components/Bottom.vue
+++ src/views/receiveMaterials/components/Bottom.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts"></script>
 <template>
   <div class="mt-330px flex items-center justify-between">
-    <div class="module"></div>
-    <div class="module"></div>
-    <div class="module"></div>
-    <div class="module"></div>
-    <div class="module"></div>
+    <div class="module">
+      <div class="text-center flex items-center justify-center mt-50px"> 车牌号 </div>
+      <div class="text-center flex items-center justify-center mt-20px color-#36d8fd font-500">
+        渝D13672
+      </div>
+    </div>
+    <div class="module">
+      <div class="text-center flex items-center justify-center mt-50px"> 材料名称 </div>
+      <div class="text-center flex items-center justify-center mt-20px color-#36d8fd font-500">
+        渝D13672
+      </div>
+    </div>
+    <div class="module">
+      <div class="text-center flex items-center justify-center mt-50px"> 进场数量:吨 </div>
+      <div class="text-center flex items-center justify-center mt-20px color-#36d8fd font-500">
+        渝D13672
+      </div>
+    </div>
+    <div class="module">
+      <div class="text-center flex items-center justify-center mt-50px">
+        <div> 毛量: </div>
+        <div class="text-center flex items-center justify-center ml-5px color-#36d8fd font-500">
+          渝D13672
+        </div>
+      </div>
+      <div class="text-center flex items-center justify-center mt-20px font-500">
+        <div> 皮重: </div>
+        <div class="text-center flex items-center justify-center ml-5px color-#36d8fd font-500">
+          渝D13672
+        </div>
+      </div>
+    </div>
+    <div class="module">
+      <div class="text-center flex items-center justify-center mt-50px"> 仓料号 </div>
+      <div class="text-center flex items-center justify-center mt-20px color-#36d8fd font-500">
+        渝D13672
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,9 +47,7 @@
     width: 292px;
     height: 179px;
     background: rgba(131, 184, 251, 0.2);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+    margin: 0 auto;
     position: relative;
     &::before {
       content: '';
