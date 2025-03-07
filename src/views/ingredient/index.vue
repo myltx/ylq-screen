91ecdf4d872a9572e41028a65b10eb74f7cc7973
LiangQianQian <1140700429@qq.com>
2025-03-07 21:20:18 +0800

    feat:配料

--- src/views/ingredient/index.vue
+++ src/views/ingredient/index.vue
@@ -2,48 +2,7 @@
   <div>
     <AutoScrollTable
       class="auto-scroll-table"
-      :headers="[
-        {
-          key: 'method',
-          title: '浇筑方式',
-          style: {
-            width: '100px',
-            textAlign: 'center',
-          },
-        },
-        {
-          key: 'num',
-          title: '设计标号',
-          style: {
-            width: '100px',
-            textAlign: 'center',
-          },
-        },
-        {
-          key: 'percent',
-          title: '设计水胶比',
-          style: {
-            width: '100px',
-            textAlign: 'center',
-          },
-        },
-        {
-          key: 'degree',
-          title: '设计塌落度',
-          style: {
-            width: '100px',
-            textAlign: 'center',
-          },
-        },
-        {
-          key: 'jd',
-          title: '预计工程量',
-          style: {
-            width: '100px',
-            textAlign: 'center',
-          },
-        },
-      ]"
+      :headers="headersTop"
       :rows="alarmList"
       height="300px"
       :class-options="{
@@ -52,11 +11,121 @@
       :scrollAuto="false"
     />
   </div>
+  <div>
+    <AutoScrollTable
+      class="auto-scroll-table"
+      :headers="headersMid"
+      :rows="alarmList"
+      height="300px"
+      :class-options="{
+        step: 0.5,
+      }"
+      :scrollAuto="false"
+    />
+  </div>
+  <!-- 第三个表格 -->
+  <div class="flex">
+    <div class="w-[20%] thead color-[#4EA4FF] text-[30px] font-medium text-center">
+      <div class="h-[80px] line-height-[80px] bgColor">材料名称</div>
+      <div class="h-[80px] line-height-[80px]">理论用量</div>
+      <div class="h-[80px] line-height-[80px] bgColor">规格型号</div>
+      <div class="h-[80px] line-height-[80px]">实际用量</div>
+      <div class="h-[80px] line-height-[80px] bgColor">含水量</div>
+    </div>
+    <div class="flex w-[80%] overflow-hidden overflow-x-scroll">
+      <div
+        class="thead color-[#4EA4FF] text-[22px] text-center"
+        v-for="(row, index) in dataList"
+        :key="index"
+      >
+        <div class="w-[200px] h-[80px] line-height-[80px] bgColor px-[12px]">{{
+          row.materialName
+        }}</div>
+        <div class="w-[200px] h-[80px] line-height-[80px] px-[12px]">{{
+          row.TheoreticalDosage
+        }}</div>
+        <div class="w-[200px] h-[80px] line-height-[80px] bgColor px-[12px]">{{
+          row.specification
+        }}</div>
+        <div class="w-[200px] h-[80px] line-height-[80px] px-[12px]">{{ row.actualUsage }}</div>
+        <div class="w-[200px] h-[80px] line-height-[80px] bgColor px-[12px]">{{
+          row.moistureContent
+        }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
   import { color } from 'echarts';
   import { ref } from 'vue';
-
+  const headersTop = ref([
+    {
+      key: 'name',
+      title: '材料名称',
+      style: {
+        width: '50%',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'num',
+      title: '规格',
+      style: {
+        width: '25%',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'percent',
+      title: '数量：吨',
+      style: {
+        width: '25%',
+        textAlign: 'center',
+      },
+    },
+  ]);
+  const headersMid = ref([
+    {
+      key: 'method',
+      title: '浇筑方式',
+      style: {
+        width: '100px',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'num',
+      title: '设计标号',
+      style: {
+        width: '100px',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'percent',
+      title: '设计水胶比',
+      style: {
+        width: '100px',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'degree',
+      title: '设计塌落度',
+      style: {
+        width: '100px',
+        textAlign: 'center',
+      },
+    },
+    {
+      key: 'jd',
+      title: '预计工程量',
+      style: {
+        width: '100px',
+        textAlign: 'center',
+      },
+    },
+  ]);
   const alarmList = ref([
     {
       method: '吊斗',
@@ -65,10 +134,192 @@
       degree: '160-200',
       jd: 11,
       style: {
+        width: '200px',
         textAlign: 'center',
-        color: 'red',
       },
-      class: ' text-center',
+    },
+  ]);
+  const dataList = ref([
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
+    },
+    {
+      materialName: '粗集料2',
+      TheoreticalDosage: 234,
+      specification: '碎石10-10mm',
+      actualUsage: 356,
+      moistureContent: 4,
+      style: {
+        width: '200px',
+        textAlign: 'center',
+      },
     },
   ]);
 </script>
+<style scoped lang="scss">
+  .bgColor {
+    background-color: rgba(78, 164, 255, 0.2);
+  }
+</style>
